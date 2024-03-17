8019af7ea62a7664b12fb88170098ad457d8b99f
Michal <113856049+R4KET@users.noreply.github.com>
2024-03-17 20:50:42 +0100

    more exercises

---
+++ TrainingV2/8_11_archived_messages.py
@@ -0,0 +1,21 @@
+def show_messages(messages, sent_messages):
+    while messages:
+        current_message = messages.pop()
+        print(f"Message: {current_message}")
+        sent_messages.append(current_message)
+        
+def send_messages(sent_messages):
+    print("\nThe following messages have been sent:")
+    for sent_message in sent_messages:
+        print(sent_message)
+
+messages = ["Hello", "You're welcome!", "Nice!", "git gud"]
+sent_messages = []
+
+show_messages(messages, sent_messages)
+send_messages(messages[:])
+
+print("\nOriginal list of messages:")
+print(messages)
+print("\nList of sent messages:")
+print(sent_messages)
